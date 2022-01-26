bdb89ca3456a79b56abe109b04b3dcbc130684e1
jachymtobias <jachymtobias@seznam.cz>
2022-01-26 13:06:20 +0100

    teplomer

--- src/main.c
+++ src/main.c
@@ -62,6 +62,8 @@ int main(void)
 {
     uint32_t time = 0;
     uint16_t ADCx;
+    uint16_t napeti;
+    uint16_t teplota;
 
     setup();
 
@@ -71,7 +73,9 @@ int main(void)
             LED_REVERSE; 
             time = milis();
             ADCx = ADC_get(ADC2_CHANNEL_4);
-            printf("%ld %d\r\n", time);
+            napeti = ((uint32_t)3300 * ADCx + 512) / 1024;
+            teplota = ((uint32_t)3300 * ADCx - 409600 + 9984) / 19968;
+            printf("ADCx = %d ; U = %dmV ; T = %d.%d °C \r\n", ADCx, napeti, teplota/10, teplota%10);
         }
 
         /*LED_REVERSE; */
